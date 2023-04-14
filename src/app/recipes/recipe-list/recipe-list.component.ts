d07f7ef3c7f171578c48d90f7ff8e4ec964d8db4
Andreas Schneider <xichtus@MacBook-Pro-von-Andreas.fritz.box>
2023-04-14 20:51:32 +0200

    test recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'this is a test description', 'https://unsplash.com/de/fotos/1YhSMc1IywE')
+    new Recipe('A Test Recipe', 'this is a test description', 'https://images.unsplash.com/photo-1504387828636-abeb50778c0c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3087&q=80')
   ];
   constructor() {
     
